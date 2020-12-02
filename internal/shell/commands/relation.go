10e24543894a523162298c66d313d6db32a72a00
i582 <i58215@voenmeh.ru>
2020-12-02 19:50:27 +0300

    internal: delete unused flag

--- internal/shell/commands/relation.go
+++ internal/shell/commands/relation.go
@@ -16,11 +16,6 @@ func Relation() *shell.Executor {
 		Help: "shows the relationship between specific classes and functions",
 		Flags: flags.NewFlags(
 			&flags.Flag{
-				Name:      "--related",
-				Help:      "related class",
-				WithValue: true,
-			},
-			&flags.Flag{
 				Name:      "--classes",
 				Help:      "target class",
 				WithValue: true,
